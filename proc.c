e7f32f62781055fa9253a30c2a98ccfb8a84c933
asafch <asaf.chelouche@gmail.com>
2016-03-23 21:30:30 +0200

    add sanity tests, excl. averages

--- proc.c
+++ proc.c
@@ -269,11 +269,49 @@ wait(void)
 }
 
 int wait2(int *retime, int *rutime, int *stime) {
-  int res = wait();
-  *retime = proc->retime;
-  *rutime = proc->rutime;
-  *stime = proc->stime;
-  return res;
+  struct proc *p;
+  int havekids, pid;
+  acquire(&ptable.lock);
+  for(;;){
+    // Scan through table looking for zombie children.
+    havekids = 0;
+    for(p = ptable.proc; p < &ptable.proc[NPROC]; p++){
+      if(p->parent != proc)
+        continue;
+      havekids = 1;
+      if(p->state == ZOMBIE){
+        // Found one.
+        *retime = p->retime;
+        *rutime = p->rutime;
+        *stime = p->stime;
+        pid = p->pid;
+        kfree(p->kstack);
+        p->kstack = 0;
+        freevm(p->pgdir);
+        p->state = UNUSED;
+        p->pid = 0;
+        p->parent = 0;
+        p->name[0] = 0;
+        p->killed = 0;
+        p->ctime = 0;
+        p->retime = 0;
+        p->rutime = 0;
+        p->stime = 0;
+        p->priority = 0;
+        release(&ptable.lock);
+        return pid;
+      }
+    }
+
+    // No point waiting if we don't have any children.
+    if(!havekids || proc->killed){
+      release(&ptable.lock);
+      return -1;
+    }
+
+    // Wait for children to exit.  (See wakeup1 call in proc_exit.)
+    sleep(proc, &ptable.lock);  //DOC: wait-sleep
+  }
 }
 
 #ifdef SML
--- sanity.c
+++ sanity.c
@@ -5,51 +5,57 @@
 int
 main(int argc, char *argv[])
 {
-	int i, n, j = 0, k;
+	int i;
+	int n;
+	int j = 0;
+	int k;
+	int retime;
+	int rutime;
+	int stime;
+	int sum_retime = 0;
+	int sum_rutime = 0;
+	int sum_stime = 0;
 	if (argc!=2){
-		    printf(1, "should get number(n) as parameter!\n");
+		    printf(1, "Usage: sanity <n>\n");
 		    exit();
 		 }
 
 	n = atoi(argv[1]);
 	i = n; //unimportant
-	int pd;
-	for(i = 0; i< 3*n; i++){
-		j = (j==2) ? 0 : 3;
-		//printf(1, "%d\n", i);
-		pd = fork();
-		if (pd == 0){//child
-			switch(j){
-				case (0): //CPU‐bound process (CPU):
-				for( k =0; k< 100; k++){
-					for (j=0; j<1000000; j++){}
-				}
-				printf(1, "yo!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!11\n");
-					break;
-				case (1): //short tasks based CPU‐bound process (S‐CPU):
-					for( k =0; k< 100; k++){
-						for (j=0; j<1000000; j++){}
-					}
-					//	yield();   														TODO GILAD
-					break;
+	int pid;
+	for (i = 0; i < 3 * n; i++) {
+		j = i % 3;
+		pid = fork();
+		if (pid == 0) {//child
+			switch(j) {
+				case 0: //CPU‐bound process (CPU):
+					for (k = 0; k < 100; k++){
+						for (j = 0; j < 1000000; j++){
 
-				case (2):// I/O bound process (IO)
-					for( k =0; k< 100; k++){
+						}
+					}
+					break;
+				case 1: //short tasks based CPU‐bound process (S‐CPU):
+					for (k = 0; k < 100; k++){
+						for (j = 0; j < 1000000; j++){
+
+						}
+					}
+					yield();
+					break;
+				case 2:// I/O bound process (IO)
+					for(k = 0; k < 100; k++){
 						sleep(1);
 					}
 					break;
 			}
-			exit();
+			exit(); // children exit here
 		}
-		if (pd>0){//father
-			continue; 
-		}
-		printf(1, "panic!!! fork falild in sanity!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n");
+		continue; // father continues to spawn the next child
 	}
-
-
-
+	for (i = 0; i < 3 * n; i++) {
+		pid = wait2(&retime, &rutime, &stime);
+		printf(1, "i: %d, pid: %d, retime: %d, rutime: %d, stime: %d\n", i, pid, retime, rutime, stime);
+	}
   exit();
 }
-
-
